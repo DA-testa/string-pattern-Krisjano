ceaf7e97a23c4cfdb81055d75763fa50a52e88fd
Krisjanis Adamsons 221REC031 <112925675+Krisjano@users.noreply.github.com>
2023-04-16 23:07:27 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,61 @@
-# python3
-
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    ievade = input()
+    if 'F' in ievade:
+        try:
+            with open("./tests/06") as fails:
+                f = fails.readlines()
+                pattern = f[0].rstrip()
+                text = f[1].rstrip()
+        except FileNotFoundError:
+            print("Fails nav atrasts")
+            return
+
+    elif 'I' in ievade:
+        pattern = input().rstrip()
+        text = input().rstrip()
+
+    else:
+        print("Nepareiza komanda")
+        return
+    return pattern, text
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    d = 256
+    q = 101
+    M = len(pattern)
+    N = len(text)
+    i = 0
+    j = 0
+    pp = 0
+    tt = 0
+    h = 1
 
-    # and return an iterable variable
-    return [0]
+    for i in range(M - 1):
+        h = (h * d) % q
 
+    for i in range(M):
+        pp = (d * pp + ord(pattern[i])) % q
+        tt = (d * tt + ord(text[i])) % q
 
-# this part launches the functions
+    occurances = []
+    for i in range(N - M + 1):
+        if pp == tt:
+            for j in range(M):
+                if text[i + j] != pattern[j]:
+                    break
+            j = j + 1
+            if j == M:
+                occurances.append(i)
+        if i < N - M:
+            tt = (d * (tt - ord(text[i]) * h) + ord(text[i + M])) % q
+            if tt < 0:
+                tt = tt + q
+    return occurances
+
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
-
+    pattern, text = read_input()
+    if pattern and text:
+        print_occurrences(get_occurrences(pattern, text))
